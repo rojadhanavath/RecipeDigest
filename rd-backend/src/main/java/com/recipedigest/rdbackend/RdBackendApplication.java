95859a4b2de514cf7d17572d0b075c79a063a991
rojadhanavath <roja.d293@gmail.com>
2019-02-09 20:56:20 -0500

    created required artifacts for recipe digest by Roja Dhanavath

---
+++ rd-backend/src/main/java/com/recipedigest/rdbackend/RdBackendApplication.java
@@ -0,0 +1,14 @@
+package com.recipedigest.rdbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdBackendApplication.class, args);
+	}
+
+}
+
---
+++ rd-backend/src/test/java/com/recipedigest/rdbackend/RdBackendApplicationTests.java
@@ -0,0 +1,17 @@
+package com.recipedigest.rdbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RdBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
